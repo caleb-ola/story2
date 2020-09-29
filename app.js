cd5ae9144ec1a0858011209a94e615b0518490e7
caleb amao <olajiire2@gmail.com>
2020-09-29 11:32:30 -0700

    Using environment variables

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //jshint esversion:6
 
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -23,8 +24,8 @@ userSchema = new mongoose.Schema ({
     password: String
 });
 
-const secret = "My deepest secret";
-userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"]});
+
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
 
 
 const User = mongoose.model("User", userSchema);
